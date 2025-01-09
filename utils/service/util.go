aa11d8773a1ad7792ce8db0189986286a1bd629d
L1cHHH (Karateev Kirill) <kirill.karateev2016@gmail.com>
2025-01-09 23:59:24 +0300

    Added utils functions for verifying user input, hashing password

---
+++ utils/service/util.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"strings"
+	"golang.org/x/crypto/bcrypt"
+)
+
+//Proves correctness of user input
+func IsCorrectFormat(input string) bool {
+	if len(input) < 8 || len(input) > 16 {
+		return false
+	}
+
+	disallowedSymbols := "#$%^&*!?~`':;_-+=<>,/|}{[]()"
+
+	if !strings.ContainsAny(input, disallowedSymbols) {
+		return true
+	} else {
+		return false
+	}
+
+}
+
+//Hashing password
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashed), nil
+}
