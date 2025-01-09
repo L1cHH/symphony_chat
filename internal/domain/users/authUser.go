d3bf976353b258aeb32d484ee2415d97c7083397
L1cHHH (Karateev Kirill) <kirill.karateev2016@gmail.com>
2025-01-09 16:33:18 +0300

    Added authUser aggregate

--- internal/domain/users/authUser.go
+++ internal/domain/users/authUser.go
@@ -1,14 +1,35 @@
 package users
 
-import "github.com/google/uuid"
+import (
+	"time"
+	"github.com/google/uuid"
+)
 
 type AuthUser struct {
 	id uuid.UUID
-	nickname string
 	login string
 	password string
+	registrationAt time.Time
 }
 
 func (au AuthUser) GetID() uuid.UUID {
 	return au.id
+}
+
+func (au AuthUser) GetLogin() string {
+	return au.login
+}
+
+func (au AuthUser) GetPassword() string {
+	return au.password
+}
+
+func (au AuthUser) GetRegistrationAt() time.Time {
+	return au.registrationAt
+}
+
+//AuthUserRepository for managing AuthUser aggregate
+
+type AuthUserRepository interface {
+
 }
