dddc3c9adaebb92b0df8725585e75b9aca94a837
L1cHHH (Karateev Kirill) <kirill.karateev2016@gmail.com>
2025-01-11 21:17:09 +0300

    Added SignUpUser (final version)

--- internal/service/users/registration_service.go
+++ internal/service/users/registration_service.go
@@ -2,7 +2,9 @@ package service
 
 import (
 	"errors"
+	"symphony_chat/internal/domain/jwt"
 	"symphony_chat/internal/domain/users"
+	jwtService "symphony_chat/internal/service/jwt"
 	utils "symphony_chat/utils/service"
 )
 
@@ -15,12 +17,15 @@ var (
 	ErrDatabaseProblem = errors.New("problem with database")
 	//Problem with hashing password
 	ErrHashingPassword = errors.New("error occurs while hashing")
+	//Problem with creating JWT token
+	ErrProblemWithJWT = errors.New("jwt error")
 	//Uncoverable
 	ErrUnimplementedError = errors.New("this error is uncovered")
 )
 
 type RegistrationService struct {
 	authUserRepo users.AuthUserRepository
+	jwtService jwtService.JWTtokenService
 }
 
 type RegistrationConfiguration func(*RegistrationService) error 
@@ -47,41 +52,55 @@ func WithAuthUserRepository(au users.AuthUserRepository) RegistrationConfigurati
 	}
 }
 
-func (rs *RegistrationService) SignUpUser(userInput users.UserDTO) error {
+func WithJWTtokenService(jwtService jwtService.JWTtokenService) RegistrationConfiguration {
+	return func(rs *RegistrationService) error {
+		rs.jwtService = jwtService
+		return nil
+	}
+}
+
+func (rs *RegistrationService) SignUpUser(userInput users.UserDTO) ([2]jwt.JWTtoken, error) {
 
 	//Validation user input
 
 	if !utils.IsCorrectFormat(userInput.Login) || !utils.IsCorrectFormat(userInput.Password) {
-		return ErrUncorrectFormatLogin
+		return [2]jwt.JWTtoken{}, ErrUncorrectFormatLogin
 	}
 
 	exists, err := rs.authUserRepo.IsUserExists(userInput.Login)
 	if err != nil {
-		return errors.New(ErrDatabaseProblem.Error() + ": " + err.Error())
+		return [2]jwt.JWTtoken{}, errors.New(ErrDatabaseProblem.Error() + ": " + err.Error())
 	}
 
 	if exists {
-		return ErrLoginAlreadyExists
+		return [2]jwt.JWTtoken{}, ErrLoginAlreadyExists
 	}
 
 	//Hashing password
 	hashedPassword, err := utils.HashPassword(userInput.Password)
 	if err != nil {
-		return errors.New(ErrHashingPassword.Error() + ": " + err.Error())
+		return [2]jwt.JWTtoken{}, errors.New(ErrHashingPassword.Error() + ": " + err.Error())
 	}
 
 	//Creating AuthUser
 	authUser := rs.CreateAuthUser(userInput.Login, hashedPassword)
-	
+
 	//Adding AuthUser to database
 	err = rs.authUserRepo.AddAuthUser(authUser)
 	if err != nil {
-		return errors.New(ErrDatabaseProblem.Error() + ": " + err.Error())
+		return [2]jwt.JWTtoken{}, errors.New(ErrDatabaseProblem.Error() + ": " + err.Error())
 	}
 
-	return nil
+	//Creating pair of jwt tokens(access and refresh)
+	jwtTokens, err := rs.jwtService.GetNewPairTokens(authUser.GetID())
+	if err != nil {
+		return [2]jwt.JWTtoken{}, errors.New(ErrProblemWithJWT.Error() + ": " + err.Error())
+	}
+
+	return jwtTokens, nil
 }
 
 func (rs *RegistrationService) CreateAuthUser(login string, password string) users.AuthUser {
-	return users.NewAuthUser(login, password)
+	authUser := users.NewAuthUser(login, password)
+	return authUser
 }
