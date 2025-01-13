23481dd8737a68a5c8f8e2cdee6d8343a5d11742
L1cHHH (Karateev Kirill) <kirill.karateev2016@gmail.com>
2025-01-14 01:51:15 +0300

    Added function CreateDirectMessage

--- internal/service/chats/chat_service.go
+++ internal/service/chats/chat_service.go
@@ -14,20 +14,23 @@ var (
 	ErrProblemWithCreatingChat = errors.New("error occurs while creating chat")
 	//Problem with creating chatUser
 	ErrProblemWithCreatingChatUser = errors.New("error occurs while creating chatUser")
+	//Problem with db
+	ErrProblemWithDB = errors.New("error occurs while working with db")
+	//Chat does not exist
+	ErrChatDoesNotExist = errors.New("chat does not exist")
 )
 
 type ChatService struct {
-	chatUserRepo users.ChatUserRepository
+	chatUserRepo  users.ChatUserRepository
 	directMsgRepo messages.DirectMessageRepository
-	chatRepo chats.ChatRepository
+	chatRepo      chats.ChatRepository
 }
 
 type ChatConfiguration func(cs *ChatService) error
 
-
 func NewChatService(configs ...ChatConfiguration) (*ChatService, error) {
 	cs := &ChatService{}
-	
+
 	for _, cfg := range configs {
 		err := cfg(cs)
 		if err != nil {
@@ -37,7 +40,6 @@ func NewChatService(configs ...ChatConfiguration) (*ChatService, error) {
 
 	return cs, nil
 }
-
 
 func (cs *ChatService) CreateChatUser(authUserID uuid.UUID, username string) (users.ChatUser, error) {
 	chatUser := users.NewChatUser(authUserID, username)
@@ -60,4 +62,23 @@ func (cs *ChatService) CreateChat(userOneID uuid.UUID, userTwoID uuid.UUID) (cha
 	return chat, nil
 }
 
+func (cs *ChatService) CreateDirectMessage(chatID uuid.UUID, senderID uuid.UUID, receiverID uuid.UUID, text string) (messages.DirectMessage, error) {
 
+	exists, err := cs.chatRepo.IsChatExists(chatID)
+	if err != nil {
+		return messages.DirectMessage{}, errors.New(ErrProblemWithDB.Error() + ": " + err.Error())
+	}
+
+	if !exists {
+		return messages.DirectMessage{}, errors.New(ErrChatDoesNotExist.Error())
+	}
+
+	dm := messages.NewDirectMessage(chatID, senderID, receiverID, text)
+
+	err = cs.directMsgRepo.AddDirectMessage(dm)
+	if err != nil {
+		return messages.DirectMessage{}, errors.New(ErrProblemWithDB.Error() + ": " + err.Error())
+	}
+
+	return dm, nil
+}
