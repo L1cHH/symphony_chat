dae62e7d86a55824d88f77a8aa907740366bd4bc
L1cHHH (Karateev Kirill) <kirill.karateev2016@gmail.com>
2025-01-13 20:51:39 +0300

    Added CreateChat functon. CreateChatUser function was refactored

--- internal/service/chats/chat_service.go
+++ internal/service/chats/chat_service.go
@@ -1,11 +1,19 @@
 package chats
 
 import (
+	"errors"
 	"symphony_chat/internal/domain/chats"
 	"symphony_chat/internal/domain/messages"
 	"symphony_chat/internal/domain/users"
 
 	"github.com/google/uuid"
+)
+
+var (
+	//Problem with creating a chat
+	ErrProblemWithCreatingChat = errors.New("error occurs while creating chat")
+	//Problem with creating chatUser
+	ErrProblemWithCreatingChatUser = errors.New("error occurs while creating chatUser")
 )
 
 type ChatService struct {
@@ -31,6 +39,25 @@ func NewChatService(configs ...ChatConfiguration) (*ChatService, error) {
 }
 
 
-func (cs *ChatService) CreateChatUser(authUserID uuid.UUID, username string) users.ChatUser {
-	return users.NewChatUser(authUserID, username)
+func (cs *ChatService) CreateChatUser(authUserID uuid.UUID, username string) (users.ChatUser, error) {
+	chatUser := users.NewChatUser(authUserID, username)
+
+	err := cs.chatUserRepo.AddChatUser(chatUser)
+	if err != nil {
+		return users.ChatUser{}, errors.New(ErrProblemWithCreatingChatUser.Error() + ": " + err.Error())
+	}
+
+	return chatUser, nil
 }
+
+func (cs *ChatService) CreateChat(userOneID uuid.UUID, userTwoID uuid.UUID) (chats.Chat, error) {
+	chat := chats.NewChat(userOneID, userTwoID)
+	err := cs.chatRepo.AddChat(chat)
+	if err != nil {
+		return chats.Chat{}, errors.New(ErrProblemWithCreatingChat.Error() + ": " + err.Error())
+	}
+
+	return chat, nil
+}
+
+
