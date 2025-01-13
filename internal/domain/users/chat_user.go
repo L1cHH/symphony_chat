60db7427c398241ae92bd25e81deab595869e9b8
L1cHHH (Karateev Kirill) <kirill.karateev2016@gmail.com>
2025-01-13 23:23:14 +0300

    Added UserStatus, createdAt, lastSeenAt  fields in ChatUser struct

--- internal/domain/users/chat_user.go
+++ internal/domain/users/chat_user.go
@@ -1,12 +1,25 @@
 package users
 
-import "github.com/google/uuid"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type ChatUser struct {
-	id uuid.UUID
-	username string
-
+	id         uuid.UUID
+	username   string
+	status     UserStatus
+	createdAt  time.Time
+	lastSeenAt time.Time
 }
+
+type UserStatus string
+
+const (
+	Online  UserStatus = "online"
+	Offline UserStatus = "offline"
+)
 
 func (ch ChatUser) GetID() uuid.UUID {
 	return ch.id
@@ -16,17 +29,32 @@ func (ch ChatUser) GetUsername() string {
 	return ch.username
 }
 
-func NewChatUser(username string) ChatUser {
+func (ch ChatUser) GetStatus() UserStatus {
+	return ch.status
+}
+
+func (ch ChatUser) GetCreatedAt() time.Time {
+	return ch.createdAt
+}
+
+func (ch ChatUser) GetLastSeenAt() time.Time {
+	return ch.lastSeenAt
+}
+
+func NewChatUser(authUSerID uuid.UUID, username string) ChatUser {
 	return ChatUser{
-		id: uuid.New(),
-		username: username, 
+		id:         authUSerID,
+		username:   username,
+		status:     Offline,
+		createdAt:  time.Now(),
+		lastSeenAt: time.Now(),
 	}
 }
 
 type ChatUserRepository interface {
-	GetChatUserByID(uuid.UUID) (ChatUser, error) 
+	GetChatUserByID(uuid.UUID) (ChatUser, error)
+	GetChatUserByUsername(string) (ChatUser, error)
 	AddChatUser(ChatUser) error
-	DeleteChatUserByID(uuid.UUID) error 
+	DeleteChatUserByID(uuid.UUID) error
 	UpdateChatUser(uuid.UUID) error
 }
-
