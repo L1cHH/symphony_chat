2dd8f2fbf2ddc835881ef732fadc09bb149bb5d7
L1cHHH (Karateev Kirill) <kirill.karateev2016@gmail.com>
2025-01-23 13:23:58 +0300

    LogIn and LogOut handlers were provided

--- internal/application/auth/http/auth_handlers.go
+++ internal/application/auth/http/auth_handlers.go
@@ -1,12 +1,14 @@
 package http
 
 import (
+	"net/http"
 	auth "symphony_chat/internal/dto/auth"
 	as "symphony_chat/internal/service/auth/authentication"
 	rs "symphony_chat/internal/service/auth/registration"
 	utils "symphony_chat/utils/service"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type AuthHandler struct {
@@ -35,8 +37,46 @@ func (ah *AuthHandler) SignUp(c *gin.Context) {
 	}
 
 	c.JSON(200, gin.H{
-		"auth_token":    tokens.AccessToken,
+		"access_token":  tokens.AccessToken,
 		"refresh_token": tokens.RefreshToken,
 	})
 
+}
+
+func (ah *AuthHandler) LogIn(c *gin.Context) {
+	var loginCredentials auth.LoginCredentials
+	if err := c.ShouldBindJSON(&loginCredentials); err != nil {
+		c.JSON(400, gin.H{"login_error": err.Error()})
+		return
+	}
+
+	tokens, err := ah.authenticationService.LogIn(loginCredentials)
+	if err != nil {
+		c.JSON(400, gin.H{"login_error": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"access_token":  tokens.AccessToken,
+		"refresh_token": tokens.RefreshToken,
+	})
+}
+
+func (ah *AuthHandler) LogOut(c *gin.Context) {
+	userID, exists := c.Get("user_id")
+	if !exists {
+		c.JSON(http.StatusInternalServerError, gin.H{"logout_error": "User id was not provided"})
+		return
+	}
+
+	err := ah.authenticationService.LogOut(userID.(uuid.UUID))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"logout_error": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"message": "Logout successful",
+		"action":  "clear_tokens",
+	})
 }
