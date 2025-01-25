4347133dd6025a44f0c677ad8ef21e12f790854f
L1cHHH (Karateev Kirill) <kirill.karateev2016@gmail.com>
2025-01-25 23:26:52 +0300

    AuthMiddleware was updated

--- internal/application/middleware/auth_middleware.go
+++ internal/application/middleware/auth_middleware.go
@@ -23,11 +23,42 @@ func AuthMiddleware(js *jwtService.JWTtokenService) gin.HandlerFunc {
 			return 
 		}
 
-		authID, err := js.ValidateAccessToken(parts[1])
-		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": fmt.Errorf("invalid token: %w", err)})
+		authID, err := js.ValidateToken(parts[1])
+		if err == nil {
+			ctx.Set("user_id", authID)
+			ctx.Next()
 			return
 		}
+
+		if err.Error() != "token is not valid" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": fmt.Errorf("validation error: %w", err).Error()})
+			return 
+		}
+
+		cookieValue, err := ctx.Cookie("refresh_token")
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "refresh token is missing"})
+			return 
+		}
+
+		authID, err = js.ValidateToken(cookieValue)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+                "error": "Authentication required",
+                "code": "AUTH_REQUIRED",
+            })
+            return
+		}
+
+		newAccessToken, err := js.GetUpdatedAccessToken(authID)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+                "error": fmt.Errorf("problem with getting new access token: %w", err).Error(),
+            })
+			return 
+		}
+
+		ctx.Header("New-Access-Token", newAccessToken.GetToken())
 
 		ctx.Set("user_id", authID)
 		ctx.Next()
